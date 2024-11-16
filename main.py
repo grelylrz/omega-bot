38eb3d1487b4d23d54aa59a24c1df79a6db8c1b5
grely <vasilevaroslav13@gmail.com>
2024-11-17 01:56:01 +0500

    Update main.py - stats

--- main.py
+++ main.py
@@ -4,11 +4,13 @@ import requests
 from datetime import datetime
 import discord
 from discord.ext import commands
+import psutil
 
 token = None
 prefix = None
 intents = discord.Intents.default()
 intents.message_content = True
+start_time = None
 
 print("File...")
 if os.path.isfile("config.json"):
@@ -30,23 +32,19 @@ else:
 def check():
     try:
         response = requests.get("http://121.127.37.17:1212/status")
-        #print(f"Response Code: {response.status_code}")  # Выводим код ответа для диагностики
+        #print(f"Response Code: {response.status_code}")
 
         if response.status_code == 200:
             try:
-                data = response.json()  # Пытаемся распарсить JSON
-                #print(f"Received Data: {data}")  # Выводим данные для диагностики
-
-                # Обработка данных с дефолтными значениями, если они отсутствуют
+                data = response.json()
+                #print(f"Received Data: {data}")
                 name = data.get("name", "Неизвестное название")
-                players = data.get("players", 0)  # По умолчанию 0 игроков
-                map_name = data.get("map", "Неизвестная карта")  # Если карта отсутствует, установим "Неизвестная карта"
-                round_id = data.get("round_id", "Неизвестный раунд")  # Если id раунда отсутствует
-                run_level = data.get("run_level", "Неизвестный уровень")  # Если уровень отсутствует
-                preset = data.get("preset", "Неизвестный режим")  # Если режим отсутствует
+                players = data.get("players", 0)
+                map_name = data.get("map", "Неизвестная карта")
+                round_id = data.get("round_id", "Неизвестный раунд")
+                run_level = data.get("run_level", "Неизвестный уровень")
+                preset = data.get("preset", "Неизвестный режим")
                 round_start_time = data.get("round_start_time", None)
-                
-                # Если время начала раунда отсутствует, установим значение по умолчанию
                 if round_start_time:
                     round_start_time_obj = datetime.fromisoformat(round_start_time.rstrip('Z'))
                 else:
@@ -75,6 +73,8 @@ def check():
 async def on_ready():
     print(f'Bot {bot.user} ready!')
     await bot.change_presence(status=discord.Status.dnd, activity=discord.Game(name="OmegeDuty, players: not initialized"))
+    global start_time
+    start_time = datetime.now()
 
 @bot.command()
 async def status(ctx):
@@ -87,7 +87,12 @@ async def status(ctx):
         preset = status['preset']
         round_start = status['round_start_time']
         players = status['players']
-        
+        if islobby == 1:
+            islobby = "Нет"
+        elif islobby == 1:
+            islobby = "Да"
+        else:
+            islobby = "idk"
         embed = discord.Embed(
             title="Статус",
             description="beta",
@@ -111,5 +116,15 @@ async def status(ctx):
         )
         embed.add_field(name="Ошибка.", value="Ошибка при получении данных (сервер активен?)", inline=True)
         await ctx.reply(embed=embed)
-
+@bot.command()
+async def stats(ctx):
+    cpu_percent = psutil.cpu_percent(interval=1)
+    memory_info = psutil.virtual_memory()
+    total_memory = memory_info.total / 1024 / 1024
+    used_memory = memory_info.used / 1024 / 1024
+    current_time = datetime.now()
+    uptime_duration = current_time - start_time
+    hours, remainder = divmod(int(uptime_duration.total_seconds()), 3600)
+    minutes, seconds = divmod(remainder, 60)
+    await ctx.send(f"Бот работает: {hours}h {minutes}mins {seconds}sec\nCPUe: {cpu_percent:.2f}%\nRAM: {used_memory:.2f}/{total_memory:.2f} MB")
 bot.run(token)
